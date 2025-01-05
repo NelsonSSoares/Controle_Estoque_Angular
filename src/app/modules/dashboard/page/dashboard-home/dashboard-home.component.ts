26454a3efb61f009517b6cf4554962e18d30a0fb
Nelson <punkpunkada@gmail.com>
2025-01-05 17:48:24 -0300

    take, takeUntil, Ondestroy lifecicle

--- src/app/modules/dashboard/page/dashboard-home/dashboard-home.component.ts
+++ src/app/modules/dashboard/page/dashboard-home/dashboard-home.component.ts
@@ -1,23 +1,26 @@
 import { MessageService } from 'primeng/api';
 import { ProductsServiceService } from './../../../../services/products/products-service.service';
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { GetAllProductsResponse } from 'src/app/models/interfaces/products/response/GetAllProductsResponse';
 import { ProductsDataTransferService } from 'src/app/shared/services/products-data-transfer.service';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard-home',
   templateUrl: './dashboard-home.component.html',
   styleUrls: ['./dashboard-home.component.scss']
 })
-export class DashboardHomeComponent {
+export class DashboardHomeComponent implements OnDestroy{
 
   public productsList: Array<GetAllProductsResponse> = [];
+  private destroy$: Subject<void> = new Subject<void>();
 
   constructor(
     private productsService: ProductsServiceService,
     private messageService: MessageService, // PRIMENG TOAST, FAZ A SNACKBAR DE NOTIFICAÇÃO
     private productsDataTransferService: ProductsDataTransferService
   ) { }
+
 
   ngOnInit(): void {
     this.getAllProductsDatas();
@@ -26,6 +29,9 @@ export class DashboardHomeComponent {
 
   getAllProductsDatas(): void {
     this.productsService.getAllProducts()
+      .pipe(
+        takeUntil(this.destroy$)
+      )
       .subscribe({
         next: (response) => {
 
@@ -46,5 +52,10 @@ export class DashboardHomeComponent {
           });
         }
       });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,20 +1,23 @@
 
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
 import { MessageService } from 'primeng/api';
+import { Subject, takeUntil } from 'rxjs';
 import { AuthRequest } from 'src/app/models/interfaces/user/auth/AuthRequest';
 import { SignUpUserResponse } from 'src/app/models/interfaces/user/SignUpUserResponse';
 import { SignUpUserResquest } from 'src/app/models/interfaces/user/SignUpUserResquest';
-import { UserService} from 'src/app/services/user/user-service.service';
+import { UserService } from 'src/app/services/user/user-service.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnDestroy {
+
+  private destroy$: Subject<void> = new Subject<void>();
 
   loginCard = true;
 
@@ -35,69 +38,80 @@ export class HomeComponent {
     private cookieService: CookieService,
     private messageService: MessageService,
     private router: Router
-   ) { }
+  ) { }
 
-  onSubmitLoginForm():void {
-    if(this.loginForm.value && this.loginForm.valid){
-     this.userService.authUser(this.loginForm.value as AuthRequest)
-     .subscribe({
-        next: (response)=>{
-          if(response){
-            this.cookieService.set('token', response?.token);
-            this.loginForm.reset();
-            this.router.navigate(['/dashboard']);
+  onSubmitLoginForm(): void {
+    if (this.loginForm.value && this.loginForm.valid) {
+      this.userService.authUser(this.loginForm.value as AuthRequest)
+      .pipe(
+        takeUntil(this.destroy$)
+      )
+        .subscribe({
+          next: (response) => {
+            if (response) {
+              this.cookieService.set('token', response?.token);
+              this.loginForm.reset();
+              this.router.navigate(['/dashboard']);
 
+              this.messageService.add({
+                severity: 'success',
+                summary: 'Logado com sucesso!',
+                detail: `Bem vindo ${response?.name}`,
+                life: 2000
+              });
+
+            }
+
+          },
+          error: (error) => {
             this.messageService.add({
-              severity: 'success',
-              summary: 'Logado com sucesso!',
-              detail: `Bem vindo ${response?.name}`,
+              severity: 'error',
+              summary: 'Erro ao logar!',
+              detail: `Email ou senha inválidos`,
               life: 2000
             });
-
+            console.log('ERRO AO LOGAR', error);
           }
-
-        },
-        error: (error)=>{
-          this.messageService.add({
-            severity: 'error',
-            summary: 'Erro ao logar!',
-            detail: `Email ou senha inválidos`,
-            life: 2000
-          });
-          console.log('ERRO AO LOGAR', error);
-        }
-     })
+        })
     }
   }
 
-  onSubmitSignupForm():void {
-   if(this.signupForm.value && this.signupForm.valid){
-    this.userService.signupUser(this.signupForm.value as SignUpUserResquest)
-    .subscribe({
-      next: (response)=>{
-        if(response){
-          alert('Usuário cadastrado com sucesso!');
-        }
-        this.signupForm.reset();
-        this.loginCard = true;
-        this.messageService.add({
-          severity: 'success',
-          summary: 'Usuario criado com sucesso!',
-          detail: `Bem vindo ${response?.name}`,
-          life: 2000
+  onSubmitSignupForm(): void {
+    if (this.signupForm.value && this.signupForm.valid) {
+      this.userService.signupUser(this.signupForm.value as SignUpUserResquest)
+      .pipe(
+        takeUntil(this.destroy$)
+      )
+        .subscribe({
+          next: (response) => {
+            if (response) {
+              alert('Usuário cadastrado com sucesso!');
+            }
+            this.signupForm.reset();
+            this.loginCard = true;
+            this.messageService.add({
+              severity: 'success',
+              summary: 'Usuario criado com sucesso!',
+              detail: `Bem vindo ${response?.name}`,
+              life: 2000
+            });
+          },
+          error: (error) => {
+            console.log('ERRO AO CADASTRAR USUÁRIO', error);
+            this.messageService.add({
+              severity: 'error',
+              summary: 'Erro ao Criar usuario!',
+              detail: `erro ao criar usuario`,
+              life: 2000
+            });
+          }
         });
-      },
-      error: (error)=>{
-       console.log('ERRO AO CADASTRAR USUÁRIO', error);
-       this.messageService.add({
-        severity: 'error',
-        summary: 'Erro ao Criar usuario!',
-        detail: `erro ao criar usuario`,
-        life: 2000
-      });
-      }
-    });
-   }
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 
 
