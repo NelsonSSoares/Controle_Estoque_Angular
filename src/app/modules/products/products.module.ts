cf51e4428587700831dc9e8eda36b3400b14abd2
Nelson <punkpunkada@gmail.com>
2025-01-06 16:26:56 -0300

    Dumb component product table

--- src/app/modules/products/products.module.ts
+++ src/app/modules/products/products.module.ts
@@ -20,11 +20,13 @@ import { RouterModule } from '@angular/router';
 import { ProductsHomeComponent } from './page/products-home/products-home.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { PRODUCTS_ROUTES } from './products.routing';
+import { ProductsTableComponent } from './components/products-table/products-table.component';
 
 
 @NgModule({
   declarations: [
-    ProductsHomeComponent
+    ProductsHomeComponent,
+    ProductsTableComponent
   ],
   imports: [
     CommonModule,
