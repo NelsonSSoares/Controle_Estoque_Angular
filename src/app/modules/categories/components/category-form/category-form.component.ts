def4b5ee3a2480812e3920e0eb7112d6461f8ce3
Nelson <punkpunkada@gmail.com>
2025-01-17 17:31:24 -0300

    implementing edit category method

--- src/app/modules/categories/components/category-form/category-form.component.ts
+++ src/app/modules/categories/components/category-form/category-form.component.ts
@@ -2,10 +2,11 @@ import { CategoryEvent } from 'src/app/models/enums/category/CategoryEvent';
 
 import { CategoriesService } from 'src/app/services/categories/categories.service';
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 import { MessageService } from 'primeng/api';
 import { DynamicDialogConfig } from 'primeng/dynamicdialog';
 import { Subject } from 'rxjs';
+import { EditCategoryAction } from 'src/app/models/interfaces/categories/event/EditCategoryAction';
 
 
 @Component({
@@ -20,7 +21,10 @@ export class CategoryFormComponent implements OnInit,OnDestroy{
   public addCategoryAction = CategoryEvent.ADD_CATEGORY_ACTION;
   public editCategoryAction = CategoryEvent.EDIT_CATEGORY_ACTION;
 
-  //  public categoryAction!: {event: EditCategoryAction};
+  public categoryAction!: {event: EditCategoryAction};
+  public categoryForm = this.formBuilder.group({
+    name: ['',Validators.required],
+  });
 
   constructor(
     private ref: DynamicDialogConfig,
@@ -30,6 +34,10 @@ export class CategoryFormComponent implements OnInit,OnDestroy{
   ) { }
 
   ngOnInit(): void {
+
+  }
+
+  handleSubmitAddCategory(): void{
 
   }
 
